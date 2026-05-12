3c6800eb5a14815a95bfabd28829c1cb50c9d1fb
Yash <kumarkyashwant@gmail.com>
2026-05-12 14:11:08 +0530

    updated changes

--- Django/new_django_api/new_django_api/settings.py
+++ Django/new_django_api/new_django_api/settings.py
@@ -147,6 +147,7 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://localhost:3000",
     "https://devfolio-frontend-z9so.onrender.com",  # your frontend Render URL
 ]
